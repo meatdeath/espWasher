d1442c1f1fc90a3333955224f997896abe51096b
Vladimir Novac <vladimir.novac@irdeto.com>
2021-11-03 08:50:37 -0400

    Finish lcd refactoring

--- src/lg240644-s8.cpp
+++ src/lg240644-s8.cpp
@@ -66,7 +66,7 @@ uchar STR_24064_7[] =
 // �����ֿ����
 //******************************************************************* 
 
-uint dis_col,dis_line;
+uint dis_col, dis_line;
 bool ch_68;
 
 
@@ -108,11 +108,11 @@ void lcd_wr_ctrl(uint8_t ctrlcode)
 
 void wr_data(uint8_t dispdata) 
 {
-    digitalWrite(LCD_CS,LOW);//LCD_CS = 0;
-    digitalWrite(LCD_CD,HIGH);//LCD_CD = 1;
+    LCD_SET_CS(LOW);
+    LCD_SET_CD(HIGH);
     lcd_SendBit(dispdata,8);
-    digitalWrite(LCD_CD,LOW);//LCD_CD = 0;
-    digitalWrite(LCD_CS,HIGH);//LCD_CS = 1;
+    LCD_SET_CD(LOW);
+    LCD_SET_CS(HIGH);
 }
 
 //*******************************************************************
@@ -120,12 +120,13 @@ void wr_data(uint8_t dispdata)
 //*******************************************************************
 
 void lcd_setcursor(uint8_t x, uint8_t y)
-	{  	
-   		lcd_wr_ctrl((x&0x0F)|0x60);	        // row address LSB
-		lcd_wr_ctrl((x>>4)|0x70);		        // row address MSB  
-		lcd_wr_ctrl((y>>4)|0x10);				// column address MSB
-		lcd_wr_ctrl((y&0x0F)|0x00);			// column address LSB
-	}
+{  	
+	lcd_wr_ctrl( LCD_CMD_SET_ROW_ADDR_LSB | (x&0xF) );	// lcd_wr_ctrl((x&0x0F)|0x60);	        // row address LSB
+	lcd_wr_ctrl( LCD_CMD_SET_ROW_ADDR_MSB | (x>>4) );	// lcd_wr_ctrl((x>>4)|0x70);		        // row address MSB 
+	
+	lcd_wr_ctrl( LCD_CMD_SET_COLUMN_ADDR_LSB | (y&0xF) );	// lcd_wr_ctrl(|0x00);			// column address LSB
+	lcd_wr_ctrl( LCD_CMD_SET_COLUMN_ADDR_MSB | (y>>4) );	// lcd_wr_ctrl(|0x10);				// column address MSB
+}
 
 //*******************************************************************
 // Clear LCD screen		
@@ -151,186 +152,215 @@ void lcd_clr_screen(void)
 // Draw square
 //*******************************************************************
 
-void dis_square(void)
-	{
-		uint16_t  x,y;
+void lcd_dis_square(void)
+{
+	uint16_t  x,y;
 
-		clr_screen();
-        setcursor(0,0);					// set cursor
-   		for(y=0;y<dis_col/2;y++) 		// write dis_col/2 column
+	lcd_clr_screen();
+	lcd_setcursor( 0, 0 );					// set cursor
+	for( y = 0; y < dis_col/2; y++ ) 		// write dis_col/2 column
+	{
+		lcd_wr_data(0xff);				// write display data 
+		lcd_wr_data(0xff);
+		lcd_wr_data(0xff);				
+	}				
+	for( x = 1; x < dis_line-1; x++ )		// write dis_line-2 row
+	{
+		lcd_setcursor( x, 0 );				// set cursor
+		lcd_wr_data(0xf0);				// write display data 
+		lcd_wr_data(0x00);				
+		lcd_wr_data(0x00);
+		for( y = 1; y < dis_col/2-1; y++ ) 	// write dis_col/2-2 column 
 		{
-			wr_data(0xff);				// write display data 
-			wr_data(0xff);
-            wr_data(0xff);				
-		}				
-		for(x=1;x<dis_line-1;x++)		// write dis_line-2 row
-		{
-			setcursor(x,0);				// set cursor
-			wr_data(0xf0);				// write display data 
-			wr_data(0x00);				
-   			wr_data(0x00);
-            for(y=1;y<dis_col/2-1;y++) 	// write dis_col/2-2 column 
-			{
-				wr_data(0x00);			// write display data
-				wr_data(0x00);
-                wr_data(0x00);				
-			}					
-			wr_data(0x00);
-            wr_data(0x00);				// write display data 
-			wr_data(0x0f);	
-		}
-		setcursor(dis_line-1,0);		// set cursor
-   		for(y=0;y<dis_col/2;y++) 		// write dis_col/2 column 
-		{
-			wr_data(0xff);				// write display data 
-			wr_data(0xff);
-            wr_data(0xff);				
-		}
+			lcd_wr_data(0x00);			// write display data
+			lcd_wr_data(0x00);
+			lcd_wr_data(0x00);				
+		}					
+		lcd_wr_data(0x00);
+		lcd_wr_data(0x00);				// write display data 
+		lcd_wr_data(0x0f);	
 	}
+	lcd_setcursor( dis_line-1, 0 );		// set cursor
+	for( y = 0; y < dis_col/2; y++ ) 		// write dis_col/2 column 
+	{
+		lcd_wr_data(0xff);				// write display data 
+		lcd_wr_data(0xff);
+		lcd_wr_data(0xff);				
+	}
+}
     
 //*******************************************************************/
 // byte extend 64k color mode			
 //*******************************************************************
 
-void byte_extend_ch(uchar dat)			
+void lcd_byte_extend_ch(uint8_t dat)			
+{
+	uint8_t i, j, dat1, dat2;
+	for( i = 0; i < 8; )					// extend D7--->D0 bit data
 	{
-		uchar i,j,dat1,dat2;
-		
-		
-		for(i=0;i<8;)					// extend D7--->D0 bit data
+		dat1 = 0;
+		dat2 = 0;
+		for( j = 0; j < 3; )				// 3 dot data extend to 2 8bit data
 		{
-			dat1=0;dat2=0;
-			for(j=0;j<3;)				// 3 dot data extend to 2 8bit data
+			if( (dat&0x80>>i) > 0 )		// first extend D7(i=0)
+				dat1 = 0xF8;				// "D7"=1; dat1=0xF8
+			else 
+				dat1=0x00;			// "D7"=0; dat1=0x00
+			j++;
+			i++;				// circle variable add 1
+			if( (dat&0x80>>i) > 0 )		// second extend D6(i=1)
 			{
-				if((dat&0x80>>i)>0)		// first extend D7(i=0)
-				dat1=0xF8;				// "D7"=1; dat1=0xF8
-				else dat1=0x00;			// "D7"=0; dat1=0x00
-				j++;i++;				// circle variable add 1
-				if((dat&0x80>>i)>0)		// second extend D6(i=1)
-				{dat1=dat1|0x07;		// "D6"=1; dat1=dat1|0x07
-				dat2=0xE0;}				// dat2=0xE0
-				else {dat1=dat1|0x00;	// "D6"=0; dat1=dat1|0x00
-					 dat2=0x00;}
-				j++;i++;				// circle variable add 1
-				if((dat&0x80>>i)>0)		// second extend D6(i=1)
-				dat2=dat2|0x1F;			// "D6"=1; dat1=dat1|0x1F
-				else dat2=dat2|0x00;	// "D6"=0; dat1=dat1|0x00
-				wr_data(dat1);			// write extend data
-				wr_data(dat2);			// write extend data
-				j++;i++;				// circle variable add 1
+				dat1 = dat1|0x07;		// "D6"=1; dat1=dat1|0x07
+				dat2 = 0xE0;			// dat2=0xE0
+			}				
+			else 
+			{
+				dat1 = dat1|0x00;	// "D6"=0; dat1=dat1|0x00
+				dat2 = 0x00;
 			}
+			j++;
+			i++;				// circle variable add 1
+			if( (dat&0x80>>i) > 0 )		// second extend D6(i=1)
+				dat2 = dat2|0x1F;			// "D6"=1; dat1=dat1|0x1F
+			else 
+				dat2 = dat2|0x00;	// "D6"=0; dat1=dat1|0x00
+			lcd_wr_data(dat1);			// write extend data
+			lcd_wr_data(dat2);			// write extend data
+			j++;
+			i++;				// circle variable add 1
 		}
 	}
+}
 
 //*******************************************************************
 // byte extend 4k color mode 6*8ch			
 //*******************************************************************
 
-void byte_extend_6(uchar dat)			
+void lcd_byte_extend_6(uint8_t dat)			
+{
+	uint8_t i, dat1;
+	for( i = 0; i < 3; i++ )			// extend D7--->D0 bit data
 	{
-		uchar i,dat1;
-		for(i=0;i<3;i++)			// extend D7--->D0 bit data
-		{
-			dat1=0x00;
-            if(dat&0x80)		    // first extend D7(i=0)
-			dat1=dat1|0xF0;			
-			else dat1=dat1|0x00;	
-			dat<<=1;
-			if(dat&0x80)		    // second extend D6(i=1)
-			dat1=dat1|0x0f;			
-			else dat1=dat1|0x00;	
-			wr_data(dat1);			// write extend data
-			dat<<=1;				
-		}
+		dat1 = 0x00;
+		if(dat&0x80)		    // first extend D7(i=0)
+			dat1 = dat1|0xF0;			
+		else 
+			dat1 = dat1|0x00;	
+		dat <<= 1;
+		if( dat&0x80 )		    // second extend D6(i=1)
+			dat1 = dat1|0x0f;			
+		else 
+			dat1 = dat1|0x00;	
+		lcd_wr_data(dat1);			// write extend data
+		dat<<=1;				
 	}
+}
     
 //*******************************************************************
 // byte extend 4k color mode 		
 //*******************************************************************
 
-void byte_extend(uchar dat)			
+void lcd_byte_extend(uint8_t dat)			
+{
+	uint8_t i, dat1;
+	for( i = 0; i < 4; i++ )			// extend D7--->D0 bit data
 	{
-		uchar i,dat1;
-		for(i=0;i<4;i++)			// extend D7--->D0 bit data
-		{
-			dat1=0x00;
-            if(dat&0x80)		    // first extend D7(i=0)
-			dat1=dat1|0xF0;			
-			else dat1=dat1|0x00;	
-			dat<<=1;
-			if(dat&0x80)		    // second extend D6(i=1)
-			dat1=dat1|0x0f;			
-			else dat1=dat1|0x00;	
-			wr_data(dat1);			// write extend data
-			dat<<=1;				
-		}
+		dat1 = 0x00;
+		if( dat&0x80 )		    // first extend D7(i=0)
+			dat1 = dat1|0xF0;			
+		else 
+			dat1 = dat1|0x00;	
+		dat <<= 1;
+		if( dat&0x80 )		    // second extend D6(i=1)
+			dat1 = dat1|0x0f;			
+		else 
+			dat1 = dat1|0x00;	
+		lcd_wr_data(dat1);			// write extend data
+		dat <<= 1;				
 	}
+}
 
 //*******************************************************************
 // display "A"			
 //*******************************************************************
 
-void byte_extend_gr(uchar dat)			
+void lcd_byte_extend_gr(uint8_t dat)			
+{
+	uint8_t i, j, k, dat1, dat2;
+	
+	for( k = 0; k < 32; )						//write 30 8bit data
 	{
-		uchar i,j,k,dat1,dat2;
-		
-		for(k=0;k<32;)						//write 30 8bit data
+		dat2 = dat;						
+		for( i = 0; i < 8; )					// extend D7--->D0 bit data
 		{
-			dat2=dat;						
-			for(i=0;i<8;)					// extend D7--->D0 bit data
+			for( j = 0; j < 2; )				// 2 dot data extend to one 8bit data
 			{
-				for(j=0;j<2;)				// 2 dot data extend to one 8bit data
-				{
-					if((dat2&0x80>>i)>0)	// first extend D7(i=0)
-					dat1=0xF0;				// "D7"=1; dat1=0xF0
-					else dat1=0x00;			// "D7"=0; dat1=0x00
-					j++;i++;				// circle variable add 1
-					if((dat2&0x80>>i)>0)	// second extend D6(i=1)
-					dat1=dat1|0x0F;			// "D6"=1; dat1=dat1|0x0F
-					else dat1=dat1|0x00;	// "D6"=0; dat1=dat1|0x00
-					wr_data(dat1);			// write extend data
-					j++;i++;				// circle variable add 1
-				}
+				if( (dat2&0x80>>i) > 0 )	// first extend D7(i=0)
+					dat1 = 0xF0;				// "D7"=1; dat1=0xF0
+				else 
+					dat1 = 0x00;			// "D7"=0; dat1=0x00
+				j++;
+				i++;				// circle variable add 1
+				if( (dat2&0x80>>i) > 0 )	// second extend D6(i=1)
+					dat1 = dat1|0x0F;			// "D6"=1; dat1=dat1|0x0F
+				else 
+					dat1 = dat1|0x00;	// "D6"=0; dat1=dat1|0x00
+				lcd_wr_data(dat1);			// write extend data
+				j++;
+				i++;				// circle variable add 1
 			}
-			k++;
 		}
+		k++;
 	}
+}
 
 //*******************************************************************
 //   �ַ��������������			
 //*******************************************************************
 
-void one_word(uchar x,uchar y,uchar *Lib,uchar ch_num,uchar widthw)
+void lcd_one_word( uint8_t x, uint8_t y, uint8_t *Lib, uint8_t ch_num, uint8_t widthw )
 	{
-		uint i,j,ch_x,ch_y,row,col;
-		int xi;							//ƫ�������ַ����ٵĿ��Զ���ΪUchar	
+		uint8_t i, j, ch_x, ch_y, row, col;
+		int xi;
 		
-		row=x;col=y;
-		xi=ch_num * widthw;				//ÿ���ַ�ȡģ���׵�ַ	
-		if (widthw==32)					    
-		{ch_x=16;ch_y=2;}				//���ְ�������,����
-		else if (widthw==24)					    
-		{ch_x=12;ch_y=2;}				//���ְ�������,����
-        else {ch_x=widthw; ch_y=1;}	    //8X16�ַ���8X8�ַ���������,����
-		for(i=0;i<ch_x;i++)				//дch_x��
+		row = x;
+		col = y;
+		xi = ch_num * widthw;	
+		if ( widthw= = 32 )					    
 		{
-			setcursor(row,col);
-			for(j=0;j<ch_y;j++)			//дch_y bit data
+			ch_x = 16;
+			ch_y = 2;
+		}
+		else if ( widthw == 24 )					    
+		{
+			ch_x = 12;
+			ch_y = 2;
+		}
+        else {
+			ch_x = widthw; 
+			ch_y = 1;
+		}
+		for( i = 0; i < ch_x; i++ )
+		{
+			lcd_setcursor( row, col );
+			for( j = 0; j < ch_y; j++ )
 			{   
-				if(ch_y==1)
+				if( ch_y == 1 )
 				{
-                    if(ch_68==0)byte_extend_6(Lib[xi]);	//д���һ�ֽ�
+                    if( ch_68 == 0 ) 
+						lcd_byte_extend_6(Lib[xi]);
                     else
                     {
                         lcd_wr_ctrl(0xd6);
-                        lcd_byte_extend_ch(Lib[xi]);	//д���һ�ֽ�
+                        lcd_byte_extend_ch(Lib[xi]);
                        	lcd_wr_ctrl(0xd5);
                     }
                 }	
-				else byte_extend(Lib[xi]);	//д���һ�ֽ�
-				xi++;				    //next bit data
+				else 
+					lcd_byte_extend(Lib[xi]);
+				xi++;
 			}								
-			row++;						//ָ���°���ַ���
+			row++;
 			col=y;	
 		}
 	}								
@@ -339,64 +369,75 @@ void one_word(uchar x,uchar y,uchar *Lib,uchar ch_num,uchar widthw)
 //	һ���ִ������			
 //*******************************************************************
 
-void dis_word(uchar x,uchar y,uchar *str,uchar ch8_16)
+void lcd_dis_word( uint8_t x, uint8_t y, uint8_t *str, uint8_t ch8_16 )
 	{
-		uint row,col,ch_x,ch_y;		  				//����ֲ�����
+		uint row,col,ch_x,ch_y;
 						    			
-		row=x;col=y;
+		row = x;
+		col = y;
 		
-		while(*str!=0x00)								//д�ַ�����'\0'����
+		while( *str != 0x00 )
 		{
-			if (*str&0x80)		      					//�ж�
+			if ( *str&0x80 )
 			{
-				if(ch8_16==12)
+				if( ch8_16 == 12 )
                 {
-                    ch_y=2;				  					//�ַ�����������
-    				ch_x=12;				  				//�ַ�����������						   
-    				one_word(row,col,hz12_tab,*str&0x7f,24);	//ֻ������7λ��дһ������
+                    ch_y = 2;
+    				ch_x = 12;					   
+    				lcd_one_word( row,col, hz12_tab, *str&0x7f, 24 );
                 }
-                else if(ch8_16==16)
+                else if( ch8_16 == 16 )
                 {
-                    ch_y=2;				  					//�ַ�����������
-    				ch_x=16;				  				//�ַ�����������						   
-    				one_word(row,col,hz16_tab,*str&0x7f,32);	//ֻ������7λ��дһ������
+                    ch_y = 2;
+    				ch_x = 16;					   
+    				lcd_one_word( row, col, hz16_tab, *str&0x7f, 32 );
                 }	
 			}
-			else if(ch8_16==6*8)		  				//ch8_16==0 8X8�ַ�/ ch8_16==1 8X16�ַ�
+			else if( ch8_16 == 6*8 )
 			{
-				ch_y=1;		   	  						//�ַ�����������
-				ch_x=8;		      						//�ַ�����������
-                ch_68=0;
-				one_word(row,col,ASC_6,*str-0x20,8);	//ascii�����0x20��ʼ
+				ch_y = 1;
+				ch_x = 8;
+                ch_68 = 0;
+				lcd_one_word( row, col, ASC_6, *str-0x20, 8 );
 			}
-			else if(ch8_16==8*8)		  					//ch8_16==0 8X8�ַ�/ ch8_16==1 8X16�ַ�
+			else if( ch8_16 == 8*8 )
 			{
-				ch_y=1;		   	  						//�ַ�����������
-				ch_x=8;		      						//�ַ�����������
-                ch_68=1;
-				one_word(row,col,ASC_8,*str-0x20,8);	//ascii�����0x20��ʼ
+				ch_y = 1;
+				ch_x = 8;
+                ch_68 = 1;
+				lcd_one_word( row, col, ASC_8, *str-0x20, 8) ;
 			}
             else
 			{
-				ch_y=1;		  	 						//�ַ�����������
-				ch_x=16;		   	 					//�ַ�����������
-			 	one_word(row,col,ASC_16,*str-0x20,16);	//ascii�����0x20��ʼ
+				ch_y = 1;
+				ch_x = 16;
+			 	lcd_one_word( row, col, ASC_16, *str-0x20, 16 );
 			}	
-			if (*str&0x80)		      					//�ж�
+			if ( *str&0x80 )
 			{
-                if(ch8_16==12)
-                col=col+ch_y*2;
-                else if(ch8_16==16)
-                col=col+ch_y*2+1;
+                if( ch8_16 == 12 )
+                	col = col+ch_y*2;
+                else if( ch8_16 == 16 )
+                	col = col+ch_y*2+1;
             }
-            else if(ch8_16==6*8)col=col+ch_y*2;
-            else col=col+ch_y*3;				     	//��һ���ַ��е�ַ
-			if (col>=dis_col)
+            else if( ch8_16 == 6*8 )
+				col = col+ch_y*2;
+            else 
+				col = col+ch_y*3;
+			if ( col >= dis_col )
             {
-                if(ch8_16==6*8){col=y;row+=ch_x+1;}
-                else {col=y;row+=ch_x;}		
+                if( ch8_16 == 6*8 )
+				{
+					col = y;
+					row += ch_x+1;
+				}
+                else 
+				{
+					col=y;
+					row+=ch_x;
+				}		
             }
-			if (row>=dis_line) row=x;		 			//�����Խ�磬��������	
+			if ( row >= dis_line ) row = x;
 			str++;	
 		}  
 	}
@@ -405,44 +446,44 @@ void dis_word(uchar x,uchar y,uchar *str,uchar ch8_16)
 // display ch	
 //*******************************************************************
 
-void dis_ch(uchar x,uchar y,uchar *str,uchar i)
-	{
-		uchar j,row,word;		  			//����ֲ�����
-		int xi;
-		
-		for (j=0;j<i;j++)		  				//��ʾ���ַ���
-		{	
-			word = str[j];						//ȡ�ַ�
-			xi=(word-0x20)*8; 					//����ģ��ַ
-			for(row=0;row<8;row++)				//д8��
-			{
-				setcursor(x+j*8+row,y);			//���ַ
-				byte_extend_gr(ASC_8[xi]);		//д��ģ����
-				xi++;
-			}
+void lcd_dis_ch( uint8_t x, uint8_t y, uint8_t *str, uint8_t i )
+{
+	uint8_t j, row, word;
+	int xi;
+	
+	for (j=0;j<i;j++)
+	{	
+		word = str[j];
+		xi = (word-0x20)*8;
+		for( row = 0; row < 8; row++ )
+		{
+			lcd_setcursor( x+j*8+row, y );
+			lcd_byte_extend_gr( ASC_8[xi] );
+			xi++;
 		}
 	}
+}
 
 //*******************************************************************/ 
 //�Աȶ�����			
 //*******************************************************************
 
-void set_contrast(uchar flag)
+void lcd_set_contrast(uint8_t flag)
+{
+	switch(flag)
 	{
-		switch(flag)
-		{
-			case 1: 
-			wr_ctrl(0x25); break;	//VOP=13.2V	    
-			case 2: 
-			wr_ctrl(0x32); break;   //VOP+0.3V  
-			case 3: 
-			wr_ctrl(0x40); break;   //VOP+0.6V  
-			case 4: 
-			wr_ctrl(0x18); break;   //VOP-0.3V  
-			case 5: 
-			wr_ctrl(0x0b); break;   //VOP-0.6V  
-		}
+	case 1: 
+		lcd_wr_ctrl(0x25); break;	//VOP=13.2V	    
+	case 2: 
+		lcd_wr_ctrl(0x32); break;   //VOP+0.3V  
+	case 3: 
+		lcd_wr_ctrl(0x40); break;   //VOP+0.6V  
+	case 4: 
+		lcd_wr_ctrl(0x18); break;   //VOP-0.3V  
+	case 5: 
+		lcd_wr_ctrl(0x0b); break;   //VOP-0.6V  
 	}
+}
     	
 //*******************************************************************/ 
 //Һ������ʼ��			
@@ -461,23 +502,23 @@ void lcd_init(void)
         digitalWrite(LCD_CS,HIGH);
 
 		delay_ms(150);
-		wr_ctrl(0x26);			//set Temp.		
-		wr_ctrl(0xA2);			//set Line Rate	
-		wr_ctrl(0xD5);			//set color mode
-		wr_ctrl(0xE9);			//set LCD bias ratio
-		wr_ctrl(0x81);	        //Electronic volume mode set
-		set_contrast(1);        // contrast value
-        wr_ctrl(0x2A);			//set power control
-		wr_ctrl(0xF1);			//set COM end
-   		wr_ctrl(0x3F);			//64  Line
-		//wr_ctrl(0xD0);		//set color pattern	BGR
-		//wr_ctrl(0xc2);		//set MX=1, SEG output reverse direction		
+		lcd_wr_ctrl(0x26);			//set Temp.		
+		lcd_wr_ctrl(0xA2);			//set Line Rate	
+		lcd_wr_ctrl(0xD5);			//set color mode
+		lcd_wr_ctrl(0xE9);			//set LCD bias ratio
+		lcd_wr_ctrl(0x81);	        //Electronic volume mode set
+		lcd_set_contrast(1);        // contrast value
+        lcd_wr_ctrl(0x2A);			//set power control
+		lcd_wr_ctrl(0xF1);			//set COM end
+   		lcd_wr_ctrl(0x3F);			//64  Line
+		//lcd_wr_ctrl(0xD0);		//set color pattern	BGR
+		//lcd_wr_ctrl(0xc2);		//set MX=1, SEG output reverse direction		
 	 							//set MY=0, COM output normal direction
-		wr_ctrl(0xD1);		    //set color pattern	RGB
-		wr_ctrl(0xc4);		    //set MX=0, SEG output normal direction		
+		lcd_wr_ctrl(0xD1);		    //set color pattern	RGB
+		lcd_wr_ctrl(0xc4);		    //set MX=0, SEG output normal direction		
 	 							//set MY=1, COM output reverse direction
-		wr_ctrl(0x89);			//set RAM address control
-		wr_ctrl(0xAf);   		//set display enable  
+		lcd_wr_ctrl(0x89);			//set RAM address control
+		lcd_wr_ctrl(0xAf);   		//set display enable  
 	}
     	
 //*******************************************************************
@@ -488,28 +529,38 @@ void lcd_main(void)
 	{
 		//P0 = P1 = P2 = 0xFF;
 
-		dis_col=80; dis_line=64; 
+		dis_col = 80; 
+		dis_line = 64; 
 		
 		lcd_init();						//��ʼ��LCD
 		while(1)
 		{ 		
-			clr_screen();
-			dis_square();
-            dis_word(8,24,STR_24064_4,6*8);
-            dis_word(24,16,STR_24064_5,8*8);
-			dis_word(40,16,STR1,16);			
+			lcd_clr_screen();
+			lcd_dis_square();
+            lcd_dis_word(8,24,STR_24064_4,6*8);
+            lcd_dis_word(24,16,STR_24064_5,8*8);
+			lcd_dis_word(40,16,STR1,16);			
             delay_ms(1000);
-			clr_screen();
-			dis_word(8,16,STR_24064_5,8*8);
-            dis_word(24,16,STR2,12);
-            dis_word(40,16,STR1,16);
+			lcd_clr_screen();
+			lcd_dis_word(8,16,STR_24064_5,8*8);
+            lcd_dis_word(24,16,STR2,12);
+            lcd_dis_word(40,16,STR1,16);
             delay_ms(800);
-            wr_ctrl(0xA7);
+            lcd_wr_ctrl(0xA7);
 			delay_ms(1000);
-            wr_ctrl(0xA6);
+            lcd_wr_ctrl(0xA6);
             clr_screen();
-			dis_ch(1,0,STR_24064_6,8);
+			lcd_dis_ch(1,0,STR_24064_6,8);
 			delay_ms(800);
+			lcd_set_contrast(2);
+			delay_ms(800);
+			lcd_set_contrast(3);
+			delay_ms(800);
+			lcd_set_contrast(4);
+			delay_ms(800);
+			lcd_set_contrast(5);
+			delay_ms(800);
+			lcd_set_contrast(1);
 		}	
 	}
 
--- src/lg240644-s8.h
+++ src/lg240644-s8.h
@@ -3,18 +3,57 @@
 
 #include "types.h"
 
-void setcursor(uchar x,uchar y);
-void clr_screen(void);						// ����
-void byte_extend(uchar dat);
-void byte_extend_6(uchar dat);
-void byte_extend_ch(uchar dat);				
-void byte_extend_hz(uchar dat);				
-void one_word(uchar x,uchar y,uchar *Lib,uchar ch_num,uchar widthw);
-void dis_word(uchar x,uchar y,uchar *str,uchar ch8_16);
+#define LCD_CMD_SET_COLUMN_ADDR_LSB                     0b00000000  // 0x00
+#define LCD_CMD_SET_COLUMN_ADDR_MSB                     0b00010000  // 0x10
+#define LCD_CMD_SET_TEMP_COMPENSATION                   0b00100100  // 0x24
+#define LCD_CMD_SET_POWER_CONTROL                       0b00101000  // 0x28
+#define LCD_CMD_SET_ADV_PROG_CONTROL                    0b00110000  // 0x30
+#define LCD_CMD_SET_SCROLL_LINE_LSB                     0b01000000  // 0x40
+#define LCD_CMD_SET_SCROLL_LINE_MSB                     0b01010000  // 0x50
+#define LCD_CMD_SET_ROW_ADDR_LSB                        0b01100000  // 0x60
+#define LCD_CMD_SET_ROW_ADDR_MSB                        0b01110000  // 0x70
+#define LCD_CMD_SET_VBIAS_POTENTIOMETER                 0b10000001  // 0x81
+#define LCD_CMD_SET_PART_DISPLAY_CTRL                   0b10000100  // 0x84
+#define LCD_CMD_SET_RAM_CONTROL                         0b10001000  // 0x88
+#define LCD_CMD_SET_FIXED_LINES                         0b10010000  // 0x90
+#define LCD_CMD_SET_LINE_RATE                           0b10100000  // 0xa0
+#define LCD_CMD_SET_ALL_PIXEL_ON                        0b10100100  // 0xa4
+#define LCD_CMD_SET_INVERS_DISPLAY                      0b10100110  // 0xa6
+#define LCD_CMD_SET_DISPLAY_ENABLE                      0b10101000  // 0xa8
+#define LCD_CMD_SET_LCD_MAPPING_CONTROL                 0b11000000  // 0xc0
+#define LCD_CMD_SET_N_LINE_INVERSION                    0b11001000  // 0xc8
+#define LCD_CMD_SET_COLOR_PATTERN                       0b11010000  // 0xd0
+#define LCD_CMD_SET_COLOR_MODE                          0b11010100  // 0xd4
+#define LCD_CMD_SET_COM_SCAN_FUNCTION                   0b11011000  // 0xd6
+#define LCD_CMD_SYSTEM_RESET                            0b11100010  // 0xe2
+#define LCD_CMD_NOP                                     0b11100011  // 0xe3
+#define LCD_CMD_SET_TEST_CONTROL                        0b11100100  // 0xe4
+#define LCD_CMD_SET_LCD_BIAS_RATIO                      0b11101000  // 0xe8
+#define LCD_CMD_SET_COM_END                             0b11110001  // 0xf1
+#define LCD_CMD_SET_PART_DISPLAY_START                  0b11110010  // 0xf2
+#define LCD_CMD_SET_PART_DISPLAY_END                    0b11110011  // 0xf3
+#define LCD_CMD_SET_WINDOW_PROGRAM_STARTING_COLUMN_ADDR 0b11110100  // 0xf4
+#define LCD_CMD_SET_WINDOW_PROGRAM_STARTING_ROW_ADDR    0b11110101  // 0xf5
+#define LCD_CMD_SET_WINDOW_PROGRAM_ENDING_COLUMN_ADDR   0b11110110  // 0xf6
+#define LCD_CMD_SET_WINDOW_PROGRAM_ENDING_ROW_ADDR      0b11110111  // 0xf7
+#define LCD_CMD_SET_WINDOW_PROGRAM_MODE                 0b11111000  // 0xf8
+#define LCD_CMD_SET_MTP_OPERATION_CONTROL               0b10111000  // 0xb8
+#define LCD_CMD_SET_MTP_WRITE_MASK                      0b10111001  // 0xb9
+#define LCD_CMD_SET_V_MTP1_POTENTIOMETER                0b11110100  // 0xf4
+#define LCD_CMD_SET_V_MTP2_POTENTIOMETER                0b11110101  // 0xf5
+#define LCD_CMD_SET_MTP_WRITE_TIMER                     0b11110110  // 0xf6
+#define LCD_CMD_SET_MTP_READ_TIMER                      0b11110111  // 0xf7
+
+
+void lcd_setcursor(uchar x,uchar y);
+void lcd_clr_screen(void);						// ����
+void lcd_byte_extend(uchar dat);
+void lcd_byte_extend_6(uchar dat);
+void lcd_byte_extend_ch(uchar dat);				
+void lcd_byte_extend_hz(uchar dat);				
+void lcd_one_word(uchar x,uchar y,uchar *Lib,uchar ch_num,uchar widthw);
+void lcd_dis_word(uchar x,uchar y,uchar *str,uchar ch8_16);
 void lcd_init(void);
 void lcd_main(void);
-// void dis_square(void);
-// void dis_small_pic(void);
-// void lcd_init2(void);
 
 #endif //__LG240644_S8_H__
